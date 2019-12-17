dcd6462b6b534f35f4d1bed80cf615ad81e4770c
Howard Smith <howard.smith@finnz.com>
2019-12-17 16:13:11 +1300

    Add ToCar extension method

--- IntroductionToLINQ_Cars/Program.cs
+++ IntroductionToLINQ_Cars/Program.cs
@@ -12,20 +12,70 @@ namespace IntroductionToLINQ_Cars
         static void Main(string[] args)
         {
             var cars = ProcessFiles("fuel.csv");
-            foreach (var car in cars)
+
+            //var query = cars.OrderByDescending(c => c.Combined)
+            //                .ThenBy(c => c.Name);
+
+            var query =
+                from car in cars
+                where car.Manufacturer == "BMW" && car.Year == 2016
+                orderby car.Combined descending, car.Name ascending
+                select car;
+
+            var top =
+                cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                    .OrderByDescending(c => c.Combined)
+                    .ThenBy(c => c.Name)
+                    .Select(c => c)
+                    .First();
+
+            Console.WriteLine(top.Name);
+
+            foreach (var car in query.Take(10))
             {
-                Console.WriteLine(car.Name);
+                Console.WriteLine($"{car.Name} : {car.Combined}");
             }
         }
 
         private static List<Car> ProcessFiles(string path)
         {
-            return 
+            //return 
+            //    File.ReadAllLines(path)
+            //    .Skip(1)
+            //    .Where(line => line.Length > 1)
+            //    .Select(Car.ParseFromCsv)
+            //    .ToList();
+
+            var query =
                 File.ReadAllLines(path)
                 .Skip(1)
-                .Where(line => line.Length > 1)
-                .Select(Car.ParseFromCsv)
-                .ToList();
+                .Where(l => l.Length > 1)
+                .ToCar();
+
+            return query.ToList();
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
         }
     }
 }
