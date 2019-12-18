832eb8099542d677dc3b5a4abdb74e2b0aaebb70
Howard Smith <howard.smith@finnz.com>
2019-12-19 09:37:52 +1300

    Add Id property to Cars.cs

--- IntroductionToLINQ_Cars/Car.cs
+++ IntroductionToLINQ_Cars/Car.cs
@@ -8,6 +8,7 @@ namespace IntroductionToLINQ_Cars
 {
     public class Car
     {
+        public int Id { get; set; }
         public int Year { get; set; }
         public string Manufacturer { get; set; }
         public string Name { get; set; }
